3ed171528702f27279efda40718fe286a238d928
WolfieKun <davidegabrielbranco@hotmail.com>
2019-05-04 03:54:18 +0000

    Added push and merge functions

--- giit.bat
+++ giit.bat
@@ -4,4 +4,29 @@ git add .
 git commit -m %2
 EXIT /B 0
 
-if %1=="commitall" CALL commitAll
+:pushCurrentBranch
+SET %branch%="dev"
+SET %remote%="origin"
+git push origin %branch%
+EXIT /B 0
+
+:pushOtherBranch
+SET %branch%="dev"
+SET %remote%="origin"
+git checkout %branch%
+git push origin %branch%
+EXIT /B 0
+
+:merge
+SET %from%=%2
+SET %to%=%3
+git checkout %from%
+git merge %to%
+git checkout %to%
+git merge %from%
+EXIT /B 0
+
+if %1=="commitAll" CALL commitAll
+if %1=="pushCurrent" CALL pushCurrentBranch
+if %1=="pushOther" CALL pushOtherBranch
+if %1=="merge" CALL merge
